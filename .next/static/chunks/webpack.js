41b0dbd9b030a3f0b099c833bd74eff7a9efd7ca
Bengi994 <88071990+Bengi994@users.noreply.github.com>
2023-11-23 16:08:49 -0500

    un poqutio de rs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2833a92a08eb8bcc"; }
+/******/ 		__webpack_require__.h = function() { return "54f429d8a4ffa9ab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
