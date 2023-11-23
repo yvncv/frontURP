242642a1dcf498f4887864178202337f5992e5f2
Bengi994 <88071990+Bengi994@users.noreply.github.com>
2023-11-23 16:16:27 -0500

    pull cambios angello pero

---
+++ src/components/SideBar/SidebarContext.tsx
@@ -0,0 +1,22 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const SidebarContext = createContext({
+  isSidebarOpen: false,
+  toggleSidebar: () => {},
+});
+
+export const useSidebarContext = () => useContext(SidebarContext);
+
+export const SidebarProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <SidebarContext.Provider value={{ isSidebarOpen, toggleSidebar }}>
+      {children}
+    </SidebarContext.Provider>
+  );
+};
