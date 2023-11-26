81aa3c0e1db67b2b04c57bd9778a4bb90da350b6
Bengi994 <88071990+Bengi994@users.noreply.github.com>
2023-11-26 01:17:59 -0500

    update react

--- src/components/SideBar/SidebarContext.tsx
+++ src/components/SideBar/SidebarContext.tsx
@@ -1,13 +1,21 @@
-import React, { createContext, useContext, useState } from 'react';
+import { type } from 'os';
+import React, { PropsWithChildren, ReactElement, ReactNode, createContext, useContext, useState } from 'react';
 
-const SidebarContext = createContext({
+interface SidebarContext {
+  isSidebarOpen: boolean,
+  toggleSidebar: () => void,
+} 
+
+const SidebarContextDefaultValue:SidebarContext = {
   isSidebarOpen: false,
   toggleSidebar: () => {},
-});
+}
+
+const SidebarContext = createContext(SidebarContextDefaultValue);
 
 export const useSidebarContext = () => useContext(SidebarContext);
-//@ts-ignore
-export const SidebarProvider = ({ children }) => {
+
+export const SidebarProvider = ({ children }:PropsWithChildren<Record<string, unknown>>):ReactElement => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const toggleSidebar = () => {
